a1181673c92689b7aefec938079d34fcbb4a2c2e
mrMekdad <mekdadr6@gmail.com>
2019-01-02 23:10:00 +0100

    data-cleanroom-desktop: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2019-data-cleanroom-desktop",
+    version="0.1.0",
+    description="Desktop data cleanup utilities for CSV harmonization and audit notes.",
+    author="Red@",
+    packages=["data_cleanroom_desktop"],
+    package_dir={"": "src"},
+)
